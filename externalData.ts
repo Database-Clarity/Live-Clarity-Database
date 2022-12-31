cc292f012a0358f1027e0d7617540d191f8084f2
Icemourne <Cboweris@gmail.com>
2022-12-31 21:23:38 +0200

    fix???

--- externalData.ts
+++ externalData.ts
@@ -3,7 +3,7 @@ import { fetchBungieManifest } from '@icemourne/tool-box'
 import { InventoryItems } from '@icemourne/tool-box/bungieInterfaces/manifest'
 import { Database } from '@icemourne/description-converter'
 
-export const database = JSON.parse(readFileSync('./intermediateDatabase.json', 'utf8')) as Database
+export const database = JSON.parse(readFileSync('./intermediateDescriptions.json', 'utf8')) as Database
 const { inventoryItems } = await fetchBungieManifest(['inventoryItems']) as { inventoryItems: InventoryItems }
 
 if (inventoryItems === undefined) throw new Error('inventoryItems is undefined')
