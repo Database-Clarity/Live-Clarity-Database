73ad511232a4b87e8856fa1d91a5805a6a1db2c6
Icemourne <77716865+Ice-mourne@users.noreply.github.com>
2023-01-01 22:44:02 +0200

    Delete externalData.ts

--- externalData.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs'
-import { fetchBungieManifest, InventoryItems } from '@icemourne/tool-box'
-import { Database } from '@icemourne/description-converter'
-
-export const database = JSON.parse(readFileSync('./liveDescriptions.json', 'utf8')) as Database
-const { inventoryItem } = await fetchBungieManifest(['inventoryItem']) as { inventoryItem: InventoryItems }
-export { inventoryItem }
