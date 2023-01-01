577a075736a7d02dba36964c8e0cbbe28d835511
Icemourne <Cboweris@gmail.com>
2023-01-01 21:36:40 +0200

    update

--- externalData.ts
+++ externalData.ts
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs'
 import { fetchBungieManifest, InventoryItems } from '@icemourne/tool-box'
 import { Database } from '@icemourne/description-converter'
 
-export const database = JSON.parse(readFileSync('./intermediateDescriptions.json', 'utf8')) as Database
+export const database = JSON.parse(readFileSync('./liveDescriptions.json', 'utf8')) as Database
 const { inventoryItem } = await fetchBungieManifest(['inventoryItem']) as { inventoryItem: InventoryItems }
 export { inventoryItem }
