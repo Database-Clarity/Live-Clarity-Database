1b3eccbb677b958f397a3d8d9380671bfb7d1cd1
Icemourne <Cboweris@gmail.com>
2023-06-03 22:01:34 +0300

    update dependencies

--- externalData.ts
+++ externalData.ts
@@ -3,10 +3,7 @@ import { readFileSync } from 'fs'
 import { Database } from '@icemourne/description-converter'
 import { fetchBungieManifest } from '@icemourne/tool-box'
 
-const data: { perks: Database['perks'] } = JSON.parse(
-   readFileSync('./liveDescriptions.json', 'utf8')
-)
+const data: { perks: Database['perks'] } = JSON.parse(readFileSync('./liveDescriptions.json', 'utf8'))
 export const database = data.perks
-debugger
 const { inventoryItem } = await fetchBungieManifest(['inventoryItem'])
 export { inventoryItem }
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 
 import {
-   DescriptionData,
-   Languages,
-   LivePerk,
-   converterSettings,
-   descriptionConverter,
-   descriptionFilter,
-   getDataFromPerk,
-   getSettings
+  DescriptionData,
+  Languages,
+  LivePerk,
+  converterSettings,
+  descriptionConverter,
+  descriptionFilter,
+  getDataFromPerk,
+  getSettings
 } from '@icemourne/description-converter'
 import { cleanObject, customJsonStringify, persistentFetch } from '@icemourne/tool-box'
 import _ from 'lodash'
@@ -16,71 +16,71 @@ import _ from 'lodash'
 import { database, inventoryItem } from './externalData.js'
 
 export const databaseConverter = (converterType: string) => {
-   const settings = getSettings(converterType)
-   const newDatabase = _.transform(database, (acc: { [key: string]: LivePerk }, perk, hash) => {
-      // first 50 are reserved for internal use only
-      if (Number(hash) < 50) return
+  const settings = getSettings(converterType)
+  const newDatabase = _.transform(database, (acc: { [key: string]: LivePerk }, perk, hash) => {
+    // first 50 are reserved for internal use only
+    if (Number(hash) < 50) return
 
-      const editorDescription = perk.editor.en?.main.trim() || ''
-      // remove perks with out description
-      if (editorDescription === '') return
-      // remove perks with bungie description
-      if (!settings.optional && editorDescription === inventoryItem?.[hash]?.displayProperties?.description) return
+    const editorDescription = perk.editor.en?.main.trim() || ''
+    // remove perks with out description
+    if (editorDescription === '') return
+    // remove perks with bungie description
+    if (!settings.optional && editorDescription === inventoryItem?.[hash]?.displayProperties?.description) return
 
-      const cleanPerk = getDataFromPerk(perk, settings.getFromPerk)
+    const cleanPerk = getDataFromPerk(perk, settings.getFromPerk)
 
-      const descriptions = Object.entries(cleanPerk.editor).reduce<{ [key: string]: any }>(
-         (acc, [language, descriptions]) => {
-            const descriptionData: DescriptionData = {
-               database,
-               descriptionString: descriptions[settings.editor],
-               editorType: settings.editor,
-               hash: cleanPerk.hash as number,
-               language: language as Languages
-            }
-            const convertedDescription = descriptionConverter(descriptionData)
-            if (convertedDescription) {
-               acc[language] = descriptionFilter(convertedDescription, converterType)
-            }
-            return acc
-         },
-         {}
-      )
+    const descriptions = Object.entries(cleanPerk.editor).reduce<{ [key: string]: any }>(
+      (acc, [language, descriptions]) => {
+        const descriptionData: DescriptionData = {
+          database,
+          descriptionString: descriptions[settings.editor],
+          editorType: settings.editor,
+          hash: cleanPerk.hash as number,
+          language: language as Languages
+        }
+        const convertedDescription = descriptionConverter(descriptionData)
+        if (convertedDescription) {
+          acc[language] = descriptionFilter(convertedDescription, converterType)
+        }
+        return acc
+      },
+      {}
+    )
 
-      acc[hash] = {
-         ...(_.omit(cleanPerk, 'editor') as any),
-         descriptions
-      }
-   })
+    acc[hash] = {
+      ...(_.omit(cleanPerk, 'editor') as any),
+      descriptions
+    }
+  })
 
-   return cleanObject(newDatabase)
+  return cleanObject(newDatabase)
 }
 
 if (!existsSync('./database')) {
-   mkdirSync('./database')
+  mkdirSync('./database')
 }
 if (!existsSync('./database/descriptions')) {
-   mkdirSync('./database/descriptions')
+  mkdirSync('./database/descriptions')
 }
 
 for (const key in converterSettings) {
-   writeFileSync(
-      `./database/descriptions/${key}.json`,
-      customJsonStringify(databaseConverter(key), ['stat', 'multiplier', 'weaponTypes', 'classNames'])
-   )
+  writeFileSync(
+    `./database/descriptions/${key}.json`,
+    customJsonStringify(databaseConverter(key), ['stat', 'multiplier', 'weaponTypes', 'classNames'])
+  )
 }
 
 type Versions = {
-   descriptions: number
+  descriptions: number
 }
 
 const version = await persistentFetch<Versions>(
-   'https://raw.githubusercontent.com/Database-Clarity/Live-Clarity-Database/live/versions.json',
-   3
+  'https://raw.githubusercontent.com/Database-Clarity/Live-Clarity-Database/live/versions.json',
+  3
 )
 const newVersion = {
-   ...version,
-   descriptions: Number((version.descriptions * 10000 + 1).toFixed(4)) / 10000
+  ...version,
+  descriptions: Number((version.descriptions * 10000 + 1).toFixed(4)) / 10000
 }
 writeFileSync('./database/versions.json', JSON.stringify(newVersion, null, 1))
 
